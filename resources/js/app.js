52128a9525221dc9ce7acbedf13bde07106b13f8
Mehriddin <mekhriddin.khalilov@gmail.com>
2024-11-22 18:23:39 +0500

    salom

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import App from "./src/App.vue";
+import {createApp} from "vue";
+
+
+
+createApp(App).mount('#app');
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.png">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/assets/plugins/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="/assets/css/feathericon.min.css">
+    <link rel="stylehseet" href="/https://cdn.oesmith.co.uk/morris-0.5.1.css">
+    <link rel="stylesheet" href="/assets/plugins/morris/morris.css">
+    <link rel="stylesheet" href="/assets/css/style.css"> </head>
+    @vite(['resources\js\app.js'])
+</head>
+<body>
+<div id="app" class="main-wrapper">
+
+</div>
+<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="/assets/js/jquery-3.5.1.min.js"></script>
+<script src="/assets/js/popper.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+<script src="/assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="/assets/plugins/raphael/raphael.min.js"></script>
+<script src="/assets/plugins/morris/morris.min.js"></script>
+<script src="/assets/js/chart.morris.js"></script>
+<script src="/assets/js/script.js"></script>
+</body>
+</html>
